413406f057b34d3a298e18083bece78b6ae556e6
tseyler <tseylerd@gmail.com>
2015-12-13 17:35:36 +0300

    multi-type input

--- src/writer.js
+++ src/writer.js
@@ -47,110 +47,27 @@ var EBML_HEADER = {
 };
 
 /**
- * @param {Array} arr
- * @param {Array} outBuffer
- * @returns {Array}
- */
-function toFlatArray(arr, outBuffer) {
-    if (!outBuffer)
-        outBuffer = new Array();
-    cycle(arr, outBuffer);
-    return outBuffer;
-};
-
-function cycle(arr, outBuffer) {
-    var len = arr.length;
-    for (var i = 0; i < len; i++)
-        processObject(arr[i], outBuffer);
-}
-
-function processObject(obj, outBuffer) {
-    if (isObject(obj)) {
-        toFlatArray(obj, outBuffer)
-    } else {
-        push(outBuffer, obj);
-    }
-}
-
-function push(outBuffer, obj) {
-    outBuffer.push(obj);
-}
-
-function isObject(obj) {
-    return typeof obj == 'object';
-}
-
-/**
  * @param {Object} data
  * @returns {String}
  */
 function makeSimpleBlock(data) {
-    var flags = 0;
-    if (data.keyframe) flags |= 128;
-    if (data.invisible) flags |= 8;
-    if (data.lacing) flags |= (data.lacing << 1);
-    if (data.discardable) flags |= 1;
-    if (data.trackNum > 127) {
-        throw "TrackNumber > 127 not supported";
-    }
+    var flags = 0 | 128;
     return [data.trackNum | 0x80, data.timecode >> 8, data.timecode & 0xff, flags].map(function (e) {
             return String.fromCharCode(e)
         }).join('') + data.frame;
 };
 
 /**
- * @param {String} riff
+ * @param {string} frame
  * @returns {Object}
  */
-function parseWebP(riff) {
-    var VP8 = riff.RIFF[0].WEBP[0];
-
-    var frame_start = VP8.indexOf('\x9d\x01\x2a');
-    for (var i = 0, c = []; i < 4; i++) {
-        c[i] = VP8.charCodeAt(frame_start + 3 + i);
-    }
-
-    var width, height, tmp;
-    tmp = (c[1] << 8) | c[0];
-    width = tmp & 0x3FFF;
-    tmp = (c[3] << 8) | c[2];
-    height = tmp & 0x3FFF;
-    return {
-        width: width,
-        height: height,
-        data: VP8,
-        riff: riff
-    }
-};
-
-/**
- * @param {String} string
- * @returns {Object}
- */
-function parseRIFF(string) {
-    var offset = 0;
-    var chunks = new Object();
-
-    while (offset < string.length) {
-        var id = string.substr(offset, 4);
-        chunks[id] = chunks[id] || [];
-        if (id == 'RIFF' || id == 'LIST') {
-            var len = parseInt(string.substr(offset + 4, 4).split('').map(function (i) {
-                var unpadded = i.charCodeAt(0).toString(2);
-                return (new Array(8 - unpadded.length + 1)).join('0') + unpadded
-            }).join(''), 2);
-            var data = string.substr(offset + 4 + 4, len);
-            offset += 4 + 4 + len;
-            chunks[id].push(parseRIFF(data));
-        } else if (id == 'WEBP') {
-            chunks[id].push(string.substr(offset + 8));
-            offset = string.length;
-        } else {
-            chunks[id].push(string.substr(offset + 4));
-            offset = string.length;
-        }
-    }
-    return chunks;
+function parseWebP(frame) {
+    var withOutHeader = frame.slice(12);
+    var index = 0;
+    while (withOutHeader.charAt(index) != ' ')
+        index++;
+    index++;
+    return { data : withOutHeader.slice(index + 4) }; //todo: add extended file type support
 };
 
 /**
@@ -171,6 +88,19 @@ function doubleToString64(num) {
         .join('')
 };
 
+/**
+ * @param {Array || string} buffer
+ * @returns {string}
+ */
+function bufferToString(buffer) {
+    if (typeof buffer == 'string')
+        return buffer;
+
+    for (var i = 0; i < buffer.length; i++) {
+        buffer[i] = String.fromCharCode(buffer[i]);
+    }
+    return buffer.join();
+}
 /**
  * @param {Number} num
  * @returns {string}
@@ -608,6 +538,52 @@ function mergeSortedLists(videoFrames, audioFrames) { // merge frames by timecod
     return result;
 };
 
+
+
+/**
+ * @param {Blob || ArrayBuffer || Array || String} object
+ * @returns {Array}
+ */
+function createAudioBuffer(object) {
+    var result;
+    if (object instanceof Blob) {
+        var fileReader = new FileReader();
+        fileReader.onload = function() {
+            result = new Uint8Array(this.result);
+        };
+        fileReader.readAsArrayBuffer(object);
+    } else if (object instanceof ArrayBuffer) {
+        result = new Uint8Array(object);
+    } else if (typeof object == 'string') {
+        result = stringToArray(object);
+    } else {
+        result = object;
+    }
+    return result;
+}
+
+/**
+ * @param {Blob || ArrayBuffer || Array || String} object
+ * @returns {Array}
+ */
+function createVideoFrame(object) {
+    var result;
+    if (object instanceof Blob) {
+        var fileReader = new FileReader();
+        fileReader.onload = function() {
+            result = this.result;
+        };
+        fileReader.readAsBinaryString(object);
+    } else if (object instanceof ArrayBuffer) {
+        result = bufferToString(new Uint8Array(object))
+    } else if (object instanceof Array) {
+        result = bufferToString(object);
+    } else {
+        result = object;
+    }
+    return result;
+}
+
 /**
  * @param {Number} width
  * @param {Number} height
@@ -620,16 +596,15 @@ var Video = function (width, height) {
     this.height = height;
     this.width = width;
     /**
-     * @param {String} frame String widthin encoded in base64 webp image
+     * @param {String || Blob || ArrayBuffer || Array} frame String widthin encoded in base64 webp image
      * @param {Number} duration
      */
     this.addVideoFrame = function (frame, duration) {
-        var webp = parseWebP(parseRIFF(frame));
+        var webp = parseWebP(createVideoFrame(frame));
         webp.timecode = this.duration;
         webp.type = FRAME_TYPE_VIDEO;
         this.duration += duration;
         this.videoFrames.push(webp);
-        //saveMemoryUsage();
     };
     /**
      * Make video
@@ -637,37 +612,24 @@ var Video = function (width, height) {
      */
     this.compile = function () {
         this.frames = mergeSortedLists(this.videoFrames, this.audioFrames);
-        //oryUsage();
         this.resultArray = this._toWebM();
-        //saveMemoryUsage();
         return this.resultArray;
     };
     /**
-     * @param {Buffer} buffer
+     * @param {Array || Blob || ArrayBuffer} audioTrack
      * @param {Number} duration
      */
-    this.addAudioTrack = function (buffer, duration) {
-        var parser = new Parser(buffer);
+    this.addAudioTrack = function (audioTrack, duration) {
+        var audioBuffer = createAudioBuffer(audioTrack);
+        var parser = new Parser(audioBuffer);
         var vorbisFile = parser.parse();
         var privateData = vorbisFile.getCodecPrivateForMatroska();
         this.audioFrames = vorbisFile.getWebMSpecificAudioPackets(duration);
         this.rate = vorbisFile.infoPacket.rate;
         this.codecPrivate = privateData;
         this.audioDuration = duration || vorbisFile.getDuration() * MS_IN_SECOND;
-        //saveMemoryUsage();
     };
-    /**
-     * @param {String} file
-     */
-    this.save = function (file) {
-        var fs = require('fs');
-        var buffer = new Array(this.resultArray.length);
-        for (var i = 0; i < buffer.length; i++) {
-            buffer[i] = this.resultArray[i];
-        }
-        fs.writeFile(file, buffer);
-        // saveMemoryUsage();
-    };
+
     this._toWebM = function () {
 
         var ebml = getEbmlStructure(this.width, this.height, Math.max(this.duration, this.audioDuration), this.codecPrivate, this.rate);
@@ -729,11 +691,7 @@ var Video = function (width, height) {
                     }
                 ].concat(clusterFrames.map(function (frame) {
                     var block = makeSimpleBlock({
-                        discardable: 0,
-                        frame: frame.type == FRAME_TYPE_VIDEO ? frame.data.slice(4) : frame.data,
-                        invisible: 0,
-                        keyframe: 1,
-                        lacing: 0,
+                        frame: frame.data,
                         trackNum: frame.type == FRAME_TYPE_VIDEO ? VIDEO_TRACK_NUMBER : AUDIO_TRACK_NUMBER,
                         timecode: Math.round(frame.timecode - clusterTimecode)
                     });
@@ -743,11 +701,9 @@ var Video = function (width, height) {
                     };
                 }))
             };
-            //saveMemoryUsage();
             segment.data.push(cluster);
             clusterTimecode += clusterDuration;
         }
-        //saveMemoryUsage();
         var position = 0;
         var cueNumber = 0;
         for (var i = 0; i < segment.data.length; i++) {
@@ -762,9 +718,6 @@ var Video = function (width, height) {
                 segment.data[i] = data;
             }
         }
-        //this.frames = null;
-        //this.audioFrames = null;
-        //this.videoFrames = null;
         return generateEBML(ebml)
     };
 };
